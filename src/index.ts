02271824e5bf1d3b9ba4dcdc068555205458706d
Samar <racitsolution@Samar.local>
2023-11-12 14:50:26 +0530

    prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "./lib/db";
 
 async function init() {
   const app = express();
@@ -12,16 +13,60 @@ async function init() {
 
   const gqlServer = new ApolloServer({
     typeDefs: `
+
+      type User{
+        id:ID
+        firstName:String
+        lastName:String
+        password:String
+        email:String
+        salt:String
+      }
         type Query {
             hello: String
             say(name:String):String
+            getUsers:[User]
         }
-    
+
+        type Mutation{
+          createUser(firstName: String!, lastName: String!, email:String!, password: String!):Boolean
+        }
     `, //Schema
     resolvers: {
       Query: {
         hello: () => `Hey ther,Iam a gql server`,
         say: (_, { name }: { name: string }) => `Hey ${name} how are you?`,
+        getUsers: async () => {
+          const allUsers = await prismaClient.user.findMany();
+          return allUsers;
+        },
+      },
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              email,
+              firstName,
+              lastName,
+              password,
+              salt: "random_salt",
+            },
+          });
+          return true;
+        },
       },
     },
   });
